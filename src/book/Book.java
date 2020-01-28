f60729ab3871bfc056ec99cb2f81f79fd5fe0895
kevingeurts <kgeurts@gmail.com>
2020-01-28 19:46:26 +0100

    Added book classes.

---
+++ src/book/Book.java
@@ -0,0 +1,20 @@
+package book;
+
+public class Book{
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+    public void borrowBook(){
+        isBorrowed = true;
+    }
+    public void returnBook(){
+        isBorrowed = false;
+    }
+    public boolean isBookBorrowed(){
+        return isBorrowed;
+    }
+}
---
+++ src/book/main.java
@@ -0,0 +1,23 @@
+package book;
+
+import contacts.ContactManager;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.notification.Failure;
+import tests.ContactTest;
+
+import javax.xml.transform.Result;
+
+class Main {
+    public static void main(String [] args){
+
+
+        Result result = (Result) JUnitCore.runClasses(ContactTest.class);
+        for (Failure failure : ((org.junit.runner.Result) result).getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+
+    }
+
+
+}
